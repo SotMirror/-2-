8c4bf238b12eb81b303d0dd72de6b472329d6898
SotMirror <110252714+SotMirror@users.noreply.github.com>
2022-08-23 22:48:14 +0300

    +

---
+++ DZ(2)/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int SecondNumber = Convert.ToInt32(number);
+if (SecondNumber > 99 )
+{
+
+    Console.WriteLine(number[2]);
+
+}
+else 
+{
+
+    Console.WriteLine("Третьей цифры нет");
+
+}
